792dc7c39a8ea451b10b9f5a06f7b7a3742f9a68
ShelpAm <shelpam@outlook.com>
2025-12-09 18:06:15 +0800

    chore: add debug message

--- src/routes/submit/+page.server.ts
+++ src/routes/submit/+page.server.ts
@@ -3,6 +3,9 @@ import type { AssignmentNew } from "$lib/types";
 
 export const load: PageLoad = async ({ fetch }) => {
     const res = await fetch("/api/assignments");
+    if (!res) {
+        throw new Error("Failed to connect to the server");
+    }
     if (!res.ok) {
         throw new Error("Failed to get /api/assignments: " + res.status);
     }
