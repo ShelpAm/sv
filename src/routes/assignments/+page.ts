d9220b67606df95dea57c08414a72be8197ad487
ShelpAm <shelpam@outlook.com>
2025-12-09 20:34:31 +0800

    fix: typo

--- src/routes/assignments/+page.ts
+++ src/routes/assignments/+page.ts
@@ -1,5 +1,5 @@
 import type { PageLoad } from './$types';
-import { fetch_assignments } from '$lib/api-call';
+import { fetch_assignments } from '$lib/api-calls';
 
 export const load: PageLoad = async ({ fetch }) => {
     const assignments = await fetch_assignments(fetch);
--- src/routes/submit/+page.ts
+++ src/routes/submit/+page.ts
@@ -1,5 +1,5 @@
+import { fetch_assignments } from "$lib/api-calls";
 import type { PageLoad } from "./$types";
-import { fetch_assignments } from "$lib/api-call";
 
 export const load: PageLoad = async ({ fetch }) => {
     const assignments = await fetch_assignments(fetch);
